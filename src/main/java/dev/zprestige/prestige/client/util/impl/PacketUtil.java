b56b58234f31073c5284fc14f48355cd4d35ed67
scaredyyy <238809861+scaredyyy@users.noreply.github.com>
2026-09-08 20:06:52 +0530

    validate packets in packet utility

--- src/main/java/dev/zprestige/prestige/client/util/impl/PacketUtil.java
+++ src/main/java/dev/zprestige/prestige/client/util/impl/PacketUtil.java
@@ -3,24 +3,20 @@ package dev.zprestige.prestige.client.util.impl;
 import dev.zprestige.prestige.client.util.MC;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class PacketUtil implements MC {
 
     public static PacketUtil INSTANCE = new PacketUtil();
 
-    public void sendPacket(Packet packet) {
-        // Legacy modules forge movement packets. Android servers reject them;
-        // standard movement still goes through Minecraft's normal sender.
-        if (PojavInput.isPojav() && packet instanceof PlayerMoveC2SPacket) {
-            return;
-        }
-        this.getMc().getNetworkHandler().sendPacket(packet);
+    public void sendPacket(Packet<?> packet) {
+        MinecraftClient client = getMc();
+        if (client.getNetworkHandler() == null) return;
+        if (!PacketStateValidator.canSend(packet)) return;
+        client.getNetworkHandler().sendPacket(packet);
     }
 
     @Override
     public MinecraftClient getMc() {
         return MinecraftClient.getInstance();
     }
-
 }
